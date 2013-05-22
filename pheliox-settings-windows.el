0e8f2df3e031d2b111cb62af15e8dc44804271fb
PHELiOX <pheliox@live.com>
2013-05-22 14:12:14 +0800

    windows dir fix

--- pheliox-settings-windows.el
+++ pheliox-settings-windows.el
@@ -1,5 +1,5 @@
 (defvar el-get-plugins-list)
-(setq el-get-plugins-list  (directory-files (concat current-emacs-path "el-get/") t)) 
+(setq el-get-plugins-list  (directory-files (concat current-emacs-path "/plugins/el-get/") t)) 
 (mapcar (lambda(plugin)
 	  (add-to-list 'load-path plugin)) el-get-plugins-list)
 ;;(add-to-list 'load-path (remove ".." (remove "." el-get-plugins-list)))
